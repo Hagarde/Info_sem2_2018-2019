c38cf4245a80c9882cd0e225731c77405cb792c1
Hagarde <43098624+Hagarde@users.noreply.github.com>
2019-03-16 19:47:46 +0100

    PLU SHAUT PLUS HAUR

--- PP/PP14/PP14_disque_tournant.py
+++ PP/PP14/PP14_disque_tournant.py
@@ -7,8 +7,35 @@ t,u = np.loadtxt('inertie_p_PP.txt',unpack=True,skiprows=1)
 # angulaire lors du 300e passage par un trou et dans theta10 la valeur de 
 # l'angle au plus proche de t=10s
 
-omega300 = 'Pas cela, malheureusement...' # A changer...
-theta10  = 'Ni cela'
+signal_montant = (max(u) + min(u)) /2#valeur à partir de laquelle ondetect le signal 
+duree = t[-1] - t[0]#duree entrze deux signaux 
+temps = [] #liste des moment au dessus valeur limite s
+#determination des moments ou il y a signal montant 
+for i in range(len(u) ): 
+    if u[i] > signal_montant : 
+        temps.append(t[i])
+
+
+compteur = 0
+for j in range (len (temps)-1) :
+    if temps [j+1] -temps [j] > duree*50:   
+        compteur +=1
+
+
+
+def theta (t) :
+    derivee = (np.pi /6) /(temps[2]-temps [1])
+    return derivee *t
+def omega (t) :
+    a =theta (temps[2]) -theta(temps[1])
+    return (a / (temps[2]-temps[1]))*t
+    
+    
+    
+    
+    
+omega300 = omega(t[300])
+theta10  = theta(10)
 
 
 # A la fin, on affiche les resultats a l'ecran
